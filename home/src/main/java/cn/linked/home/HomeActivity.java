8213c03cc6e7989872b4bab668e889b4e0116022
jiangshenglin <758481447@qq.com>
2020-07-16 21:31:19 +0800

    new project

---
+++ home/src/main/java/cn/linked/home/HomeActivity.java
@@ -0,0 +1,19 @@
+package cn.linked.home;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ mine/src/main/java/cn/linked/mine/MineFragment.java
@@ -0,0 +1,7 @@
+package cn.linked.mine;
+
+import androidx.fragment.app.Fragment;
+
+public class MineFragment extends Fragment {
+
+}
